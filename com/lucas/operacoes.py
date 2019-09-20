92402655728d3756cdcb7bd0767e50fb16007ede
lucasdpiza <lucasdpiza@hotmail.com>
2019-09-20 10:09:37 +0800

    programa multi funcional

--- com/lucas/operacoes.py
+++ com/lucas/operacoes.py
@@ -3,13 +3,7 @@ class Operacoes():
 
 
     def multiplicacao(self, valores):
-        val = 0
+        val = 1
         for v in valores:
             val = val * v
-        return val
-
-    def divisao(self, valores):
-        val = 0
-        for v in valores:
-            val = val / v
         return val
--- com/lucas/test/test_operacoes.py
+++ com/lucas/test/test_operacoes.py
@@ -7,7 +7,3 @@ class TestOperacaoes(TestCase):
     
     def test_multiplicacao(self):
         self.assertEqual(self.operacoes.multiplicacao([5,5]), 25, "Deveria ser 25")
-        
-    def test_divisao(self):
-        self.assertEqual(self.operacoes.divisao([25,5]), 5, "Deveria ser 5")
-        
